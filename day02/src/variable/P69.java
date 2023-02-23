6c41d5ec035ed70cc080297c2439660d7c2cb8b0
lssy2k1 <lssy2k1@gmail.com>
2023-02-23 18:20:50 +0900

    add email substring

--- day02/src/variable/P69.java
+++ day02/src/variable/P69.java
@@ -23,11 +23,14 @@ public class P69 {
 		String str3 = str.substring(1, 3);
 		System.out.println(str3);
 
-		String email = "jmlee@tonesol.com";
+		String email = "jmlee3838@sontonesol.com";
 		String id = "";
 		String domain = "";
-		id = email.substring(0, 5);
-		domain = email.substring(6, 17);
+		//id = email.substring(0, 5);
+		//domain = email.substring(6, 17);
+		id = email.substring(0, email.indexOf('@'));
+		domain = email.substring(email.indexOf('@') + 1, email.length());
+		
 		
 		System.out.printf("%s, %s",id, domain);
 	}
