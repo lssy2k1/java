49dc668ec7affa02d6057af3c759d3bab1743b57
lssy2k1 <lssy2k1@gmail.com>
2023-02-23 09:50:46 +0900

    2023-02-23 java day02

---
+++ day02/src/day02/p55.java
@@ -0,0 +1,13 @@
+package day02;
+
+public class p55 {
+
+	public static void main(String[] args) {
+		int a = 2000000000;
+		int b = 2000000000;
+		long A = a;
+		long B = b;
+		long result = (long)a + (long)b;//A + B;
+		System.out.println(result);
+	}
+}
---
+++ day02/src/variable/P54.java
@@ -0,0 +1,18 @@
+package variable;
+
+public class P54 {
+//ctrl space : 자동완성
+	public static void main(String[] args) {
+		//boolean
+		boolean b1 = true;
+		boolean b2 = 10 > 5;
+		//정수
+		int n1 = 10;
+			//long n2 = 200000000000;
+			//정수의 기본형은 int. 오른쪽은 int. 따라서 오류.
+		long n2 = 200000000000L;
+			//long n2 = 0L; n2 = 2222222222L;
+			//(데이터베이스에서 카운트 된 것을 받아올 것)
+	}
+
+}
