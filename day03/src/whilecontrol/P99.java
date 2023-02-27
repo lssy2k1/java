6caefd2ba04564f22fbe56891a0479b2ffa0ccfe
lssy2k1 <lssy2k1@gmail.com>
2023-02-27 09:44:51 +0900

    back

--- day03/src/whilecontrol/P99.java
+++ day03/src/whilecontrol/P99.java
@@ -4,12 +4,6 @@ public class P99 {
 
 	public static void main(String[] args) {
 		
-		int bb = 200;
-		int a = 10;
-		int b = 20;
-		int aa= 100;
-		int cc = 100;
-		int c = 100;
 		for (int i = 2; i < 10; i++) {
 			System.out.printf("%d 단 \n", i);
 			for (int j = 1; j < 10; j++) {
