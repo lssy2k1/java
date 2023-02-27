fb86a06eb5895d319d7e62d6523304c60dd8579e
lssy2k1 <lssy2k1@gmail.com>
2023-02-27 10:13:39 +0900

    0227

--- day03/src/array/P102.java
+++ day03/src/array/P102.java
@@ -1,6 +1,6 @@
 package array;
 
-public class Blank {
+public class P102 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- day03/src/whilecontrol/P99.java
+++ day03/src/whilecontrol/P99.java
@@ -4,11 +4,17 @@ public class P99 {
 
 	public static void main(String[] args) {
 		
+		
+		myfor:
 		for (int i = 2; i < 10; i++) {
 			System.out.printf("%d 단 \n", i);
 			for (int j = 1; j < 10; j++) {
-				System.out.printf("%d * %d = %d \n", i, j, (i * j));
+				System.out.printf("%d * %d = %d \t", i, j, (i * j));
+				if ((i * j) == 21) {
+					break myfor;//myfor:로 되어있는 for문을 모두 빠져나오겠습니다.
+				}
 			}
+			System.out.println("");
 		}
 
 	}
