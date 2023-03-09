76214cc9f8e7aa8edd55ce5fef55d00879939cee
lssy2k1 <lssy2k1@gmail.com>
2023-03-09 16:42:47 +0900

    java 0309

---
+++ day11/src/com/kbstar/dao/AccountDAO.java
@@ -0,0 +1,69 @@
+package com.kbstar.dao;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+
+import com.kbstar.dto.AccountDTO;
+import com.kbstar.frame.DAO;
+
+public class AccountDAO implements DAO<String, AccountDTO>{
+
+	HashMap<String, AccountDTO> db;
+	
+	public AccountDAO() {
+		db = new HashMap<>();
+	}
+	
+	@Override
+	public void insert(AccountDTO v) throws Exception {
+		if(db.containsKey(v.getAccNum())) {
+			throw new Exception("같은 계좌가 존재합니다.");
+		}
+		db.put(v.getAccNum(), v);
+	}
+
+	@Override
+	public void delete(String k) throws Exception {
+		if(!db.containsKey(k)) {
+			throw new Exception("입력하신 계좌는 없는 계좌입니다.");
+		}
+		db.remove(k);
+	}
+
+	@Override
+	public void update(AccountDTO v) throws Exception {
+		if(!db.containsKey(v.getAccNum())) {
+			throw new Exception("입력하신 계좌는 없는 계좌입니다.");
+		}
+		db.put(v.getAccNum(), v);
+	}
+
+	@Override
+	public AccountDTO select(String k) throws Exception {
+		if(!db.containsKey(k)) {
+			throw new Exception("입력하신 계좌는 없는 계좌입니다.");
+		}
+		AccountDTO obj= null;
+		obj = db.get(k);
+		return obj;
+	}
+
+	@Override
+	public List<AccountDTO> select() throws Exception {
+		if(db == null) {
+			throw new Exception("보유중인 계좌가 없습니다.");
+		}
+		List<AccountDTO> list = new ArrayList<AccountDTO>();
+		Collection<AccountDTO> col;
+		col = db.values();
+		for (AccountDTO obj : col) {
+			list.add(obj);
+		}
+		return list;
+	}
+
+
+
+}
---
+++ day11/src/com/kbstar/frame/Msg.java
@@ -0,0 +1,10 @@
+package com.kbstar.frame;
+
+public class Msg {
+	//메세지도 별도고 관리하기 위해 만듬.
+	//properties를 이용해도 된다.
+	public static String registerMsg1 = "가입을 축하합니다.";
+	public static String registerMsg2 = "가입을 진심으로 축하합니다.";
+	public static String removeMsg1 = "탈퇴를 축하합니다.";
+	public static String removeMsg2 = "탈퇴를 진심으로 축하합니다.";
+}
